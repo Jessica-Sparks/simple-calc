6fbef1cb7702703df86e005110f669c9b1f72d78
Jessica-Sparks <115658126+Jessica-Sparks@users.noreply.github.com>
2023-06-07 10:34:04 -0500

    Add files via upload

---
+++ simplecalc.py
@@ -0,0 +1,18 @@
+# Building a simple calculator
+# use the input func to ask a user for an input
+
+first_num = float(input('Please Enter a number: '))
+second_num = float(input('Please Enter another number: '))
+
+# addition
+print(first_num + second_num)
+# subtraction
+print(first_num - second_num)
+# multiplication
+print(first_num * second_num)
+# division
+print(first_num / second_num)
+# floor division
+print(first_num // second_num)
+# modulus division
+print(first_num % second_num)
